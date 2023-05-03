e1f3b6d7647287a57c1398b0d11f2031c783af02
abdulwahabkdi <30790171+abdulwahabkdi@users.noreply.github.com>
2023-05-03 16:12:56 +0100

    improved script by adding validation
    Added following validation checks
    1. The email address is in proper format
    2.  Checks if the source domain already present on iredmail or not

--- add_forwarding.sh
+++ add_forwarding.sh
@@ -1,16 +1,37 @@
 #!/bin/bash
 
+# Function to validate email address
+validate_email() {
+    # Check if email address matches the correct format
+    if [[ ! $1 =~ ^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$ ]]; then
+        echo "Invalid email address format. Please try again."
+        exit 1
+    fi
+}
+
 # Prompt the user to enter the source email address
 read -p "Enter the source email address: " SOURCE_EMAIL
 
+# Validate the source email address
+validate_email "$SOURCE_EMAIL"
+
 # Extract the domain and dest_domain from the source email address
-SOURCE_DOMAIN=$(echo $SOURCE_EMAIL | cut -d "@" -f 2)
+SOURCE_DOMAIN=$(echo "$SOURCE_EMAIL" | cut -d "@" -f 2)
+
+# Check if SOURCE_DOMAIN exists in the domain table
+if ! mysql vmail -e "SELECT domain FROM domain WHERE domain='$SOURCE_DOMAIN'" | grep -q "$SOURCE_DOMAIN"; then
+    echo "The domain '$SOURCE_DOMAIN' does not exist in the iRedmail database, please add the domain first to iRedmail before adding the forwarder."
+    exit 1
+fi
 
 # Prompt the user to enter the destination email address
 read -p "Enter the destination email address: " DESTINATION_EMAIL
 
+# Validate the destination email address
+validate_email "$DESTINATION_EMAIL"
+
 # Extract the domain and dest_domain from the destination email address
-DEST_DOMAIN=$(echo $DESTINATION_EMAIL | cut -d "@" -f 2)
+DEST_DOMAIN=$(echo "$DESTINATION_EMAIL" | cut -d "@" -f 2)
 
 # Print the variables for troubleshooting
 echo "SOURCE_EMAIL: $SOURCE_EMAIL"
@@ -19,10 +40,16 @@ echo "DESTINATION_EMAIL: $DESTINATION_EMAIL"
 #echo "DEST_DOMAIN: $DEST_DOMAIN"
 
 # Connect to the vmail database
-mysql  vmail << EOF
+mysql vmail << EOF
 INSERT INTO forwardings (address, forwarding, domain, dest_domain, is_forwarding, active)
-VALUES ('$SOURCE_EMAIL', '$DESTINATION_EMAIL', '$SOURCE_DOMAIN', '$DEST_DOMAIN', 1, 1);
+SELECT '$SOURCE_EMAIL', '$DESTINATION_EMAIL', '$SOURCE_DOMAIN', '$DEST_DOMAIN', 1, 1 FROM domain WHERE domain='$SOURCE_DOMAIN';
 EOF
 
-# Print completion message
-echo "Email forwarding from $SOURCE_EMAIL to $DESTINATION_EMAIL has been added."
+# Check if the forwarding was added successfully
+if [ $? -eq 0 ]; then
+    # Print completion message
+    echo "Email forwarding from $SOURCE_EMAIL to $DESTINATION_EMAIL has been added."
+else
+    # Print error message
+    echo "Failed to add email forwarding from $SOURCE_EMAIL to $DESTINATION_EMAIL."
+fi
